9839859cf937a0d82ab08875e734b2a476aeb3da
Jeff Lu <jeff861208@gmail.com>
2022-09-12 14:09:44 +0800

    replace artists image link with local file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1311eae84290eac9"; }
+/******/ 		__webpack_require__.h = function() { return "ae86777544e6e142"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
